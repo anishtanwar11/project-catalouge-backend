ba6db956e686ac37847ced9890443dd5807ac979
Anish <anishtanvar@gmail.com>
2024-04-06 12:37:03 +0530

    update cors

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(fileUpload({
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   })
