b0de5cd9a817ca7f42506587a2338909d8f74e98
Anish <anishtanvar@gmail.com>
2024-04-06 15:20:56 +0530

    add header function

--- index.js
+++ index.js
@@ -39,9 +39,17 @@ passport.deserializeUser((username, done) => {
 });
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(fileUpload({
   useTempFiles:true
 }))
+
 app.use(express.json());
 app.use(
   cors({
